5012360d107f419b3bec9b5dc4c42c677008ed78
Wayne Duran <asartalo@gmail.com>
2015-10-23 15:02:52 +0800

    Migrations fixing again

--- db/migrate/20151023035400_create_occasion_drinks.rb
+++ db/migrate/20151023035400_create_occasion_drinks.rb
@@ -3,7 +3,7 @@ class CreateOccasionDrinks < ActiveRecord::Migration
     create_table :occasion_drinks do |t|
       t.belongs_to :user, index: true, foreign_key: true
       t.belongs_to :drink, index: true, foreign_key: true
-      t.belongs_to :ocassion, index: true, foreign_key: true
+      t.belongs_to :occasion, index: true, foreign_key: true
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,19 @@ ActiveRecord::Schema.define(version: 20151023053457) do
 
   add_index "drinks", ["category_id"], name: "index_drinks_on_category_id"
 
+  create_table "occasion_drinks", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "drink_id"
+    t.integer  "occasion_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "amount"
+  end
+
+  add_index "occasion_drinks", ["drink_id"], name: "index_occasion_drinks_on_drink_id"
+  add_index "occasion_drinks", ["occasion_id"], name: "index_occasion_drinks_on_occasion_id"
+  add_index "occasion_drinks", ["user_id"], name: "index_occasion_drinks_on_user_id"
+
   create_table "occasions", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "title"
@@ -50,19 +63,6 @@ ActiveRecord::Schema.define(version: 20151023053457) do
   end
 
   add_index "occasions", ["user_id"], name: "index_occasions_on_user_id"
-
-  create_table "occasion_drinks", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "drink_id"
-    t.integer  "ocassion_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "amount"
-  end
-
-  add_index "occasion_drinks", ["drink_id"], name: "index_occasion_drinks_on_drink_id"
-  add_index "occasion_drinks", ["ocassion_id"], name: "index_occasion_drinks_on_ocassion_id"
-  add_index "occasion_drinks", ["user_id"], name: "index_occasion_drinks_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name",                   default: "",  null: false
