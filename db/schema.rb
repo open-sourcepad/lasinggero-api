95762c30591696e04f420863183aa63c35062ee4
Wayne Duran <asartalo@gmail.com>
2015-10-23 14:59:44 +0800

    Fixing migrations

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,16 @@ ActiveRecord::Schema.define(version: 20151023053457) do
 
   add_index "drinks", ["category_id"], name: "index_drinks_on_category_id"
 
+  create_table "occasions", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.string   "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "occasions", ["user_id"], name: "index_occasions_on_user_id"
+
   create_table "occasion_drinks", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "drink_id"
@@ -53,16 +63,6 @@ ActiveRecord::Schema.define(version: 20151023053457) do
   add_index "occasion_drinks", ["drink_id"], name: "index_occasion_drinks_on_drink_id"
   add_index "occasion_drinks", ["ocassion_id"], name: "index_occasion_drinks_on_ocassion_id"
   add_index "occasion_drinks", ["user_id"], name: "index_occasion_drinks_on_user_id"
-
-  create_table "occasions", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "title"
-    t.string   "location"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "occasions", ["user_id"], name: "index_occasions_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name",                   default: "",  null: false
